ad86e51fc448339cf6c2c09cdb6b4bc34dbab097
Ryan Logan <ryan_plogan@yahoo.com>
2025-05-26 11:07:21 -0700

    testing timeout

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ form.addEventListener('submit', function(event) {
   // Perform form submission logic here (e.g., using fetch or AJAX)
 
   // Redirect to a new page after successful submission
-  window.location.href = 'index.html'; // Using window.location.href
+  setTimeout(() => {
+   window.location.href = 'index.html'; 
+}, "5000"); // Redirect after 1 second
   // Or
   // window.location.replace('new_page.html'); // Using window.location.replace()
 });
