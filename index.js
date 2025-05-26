907a5a2b103b79df6f983179f46edb4bfd1474ea
Ryan Logan <ryan_plogan@yahoo.com>
2025-05-26 11:13:00 -0700

    trying to redirect to home page after form submission

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const form = document.getElementById('form');
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent the default form submission
-
-  // Perform form submission logic here (e.g., using fetch or AJAX)
-
-  // Redirect to a new page after successful submission
-  setTimeout(() => {
-   window.location.href = 'index.html'; 
-}, "5000"); // Redirect after 1 second
-  // Or
-  // window.location.replace('new_page.html'); // Using window.location.replace()
-});
