a777802d4ca76f7421fbdbf723145411fad1f261
Ryan Logan <ryan_plogan@yahoo.com>
2025-05-26 17:02:30 -0700

    stopped using nodemailer and/or formsubmit in lieu of using email.js. Worked successfully

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function sendMail(){
+    var params = {
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value    
+    }
 
+
+const serviceID = "service_m9qvj1s"; 
+const templateID = "template_p46ne8g";
+
+emailjs.send(serviceID, templateID, params)
+.then(
+    res => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message has been sent successfully!");
+    })
+    .catch((err) => console.log(err));
+}
