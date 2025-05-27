2c1f42290c111556d86eb31178fdce2be13b88bc
Ryan Logan <ryan_plogan@yahoo.com>
2025-05-27 07:02:55 -0700

    replaced hard coded HTML with JS so adding more images to the UL in the homepage will be less time consuming

---
+++ home.js
@@ -0,0 +1,22 @@
+var arr = []; 
+var horizontal = "Tenants_09.png";
+var vertical = "Tenants_01.jpg";
+var live = "IMG_5728.jpeg";
+var michi = "IMG_4705.jpg";
+var nate = "IMG_4754 (1).jpg";
+var ryan = "TENANTS033.jpg";
+
+arr.push(horizontal, vertical, live, michi, nate, ryan);
+
+var imageGallery = document.getElementById("image-gallery")
+
+for (var i = 0; i < arr.length; i++) {
+    var li = document.createElement("li");
+    var img = document.createElement("img");
+    img.src = arr[i];
+    img.alt = "Image " + (i + 1);
+    img.className = "image";
+    li.appendChild(img);
+    imageGallery.appendChild(li);
+}
+
